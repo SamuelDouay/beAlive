6e4770848539cc4d541f28b1929dfdb8f43bfde2
Samuel Douay <douaysamuel@gmail.com>
2022-06-27 21:10:54 +0200

    update controller

---
+++ back/src/database/messagerie/groupe/groupe.controller.ts
@@ -0,0 +1,28 @@
+import { Next, Request } from "restify";
+
+export default class GroupeController {
+
+    public static async create(req: Request, res: Response, next: Next) {
+        
+    }
+
+    public static async insert(req: Request, res: Response, next: Next) {
+
+    }
+
+    public static async update(req: Request, res: Response, next: Next) {
+
+    }
+
+    public static async delete(req: Request, res: Response, next: Next) {
+
+    }
+
+    public static async get(req: Request, res: Response, next: Next) {
+        
+    }
+
+    public static async getAll(req: Request, res: Response, next: Next) {
+
+    }
+}
