28958cc0c30081b7be6378d1ae15dc52c110e75a
Samuel Douay <65333968+SamuelDouay@users.noreply.github.com>
2022-06-28 07:40:33 +0200

    Update webpack.config.js

--- back/webpack.config.js
+++ back/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = [
           loader: "ts-loader",
           options: {
             configFile: "tsconfig.json",
+            "projectReferences": true
           },
         },
       ],
