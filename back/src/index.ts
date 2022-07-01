03e01b2cee6a5f09f4b912a0251242e082aca161
Samuel Douay <douaysamuel@gmail.com>
2022-07-02 00:14:15 +0200

    test

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,6 +2,8 @@ require('dotenv').config();
 import API from "./server/api";
 import { ApiServer } from "./server/constante/index";
 
+
+
 const server = new ApiServer();
 server.start(process.env.PORT);
 API.applyRoutes(server.getServer());
