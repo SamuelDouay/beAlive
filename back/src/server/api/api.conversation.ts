bb61e4a0a370ada14ae806386a57f2387508a196
Samuel Douay <douaysamuel@gmail.com>
2022-07-01 20:29:23 +0200

    update api [conversation, groupe] files

--- back/src/server/api/api.conversation.ts
+++ back/src/server/api/api.conversation.ts
@@ -1,9 +1,20 @@
 import { Router } from "restify-router";
+import ConversationController from "../../database/messagerie/conversation/conversation.controller";
+import { checkToken } from "../../middlewares/token.middlesware";
 
 const apiConversation: Router = new Router();
 
 apiConversation.group('/conv', (router: Router) => {
 
+    router.get("/", checkToken, ConversationController.getAll);
+
+    router.get("/:id", checkToken, ConversationController.get);
+
+    router.del("/:id", checkToken, ConversationController.delete);
+
+    router.post("/", checkToken, ConversationController.insert);
+
+    router.put("/:id", checkToken, ConversationController.update);
 });
 
 export default apiConversation;
