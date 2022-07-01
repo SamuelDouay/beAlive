252e34191c4f59e7b2c46fc69d8d7f55127bbb02
Samuel Douay <douaysamuel@gmail.com>
2022-07-01 20:29:23 +0200

    update api [conversation, groupe] files

--- back/src/server/api/api.groupe.ts
+++ back/src/server/api/api.groupe.ts
@@ -1,9 +1,20 @@
 import { Router } from "restify-router";
+import GroupeController from "../../database/messagerie/groupe/groupe.controller";
+import { checkToken } from "../../middlewares/token.middlesware";
 
 const apiGroupe: Router = new Router();
 
 apiGroupe.group('/conv', (router: Router) => {
 
+    router.get("/", checkToken, GroupeController.getAll);
+
+    router.get("/:id", checkToken, GroupeController.get);
+
+    router.del("/:id", checkToken, GroupeController.delete);
+
+    router.post("/", checkToken, GroupeController.create);
+
+    router.put("/:id", GroupeController.update);
 });
 
 export default apiGroupe;
