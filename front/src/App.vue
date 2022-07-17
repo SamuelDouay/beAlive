db1bedf32e6030619d6815db71aa4952d064a14b
Samuel Douay <douaysamuel@gmail.com>
2022-07-17 16:50:52 +0200

    fix header and footer position

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/login">Login</RouterLink>
-    </nav>
+    <div id="header">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+    </div>
   </header>
-  <RouterView />
-  <footer>Footer</footer>
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+    <div id="footer">Footer</div>
+  </footer>
 </template>
